680633031e4da59b7eb85fe5817736537e52f0cd
faltemi <michael.falter5@gmail.com>
2025-08-17 15:28:03 -0400

    Removed makefile and include + lib

--- src/main.c
+++ src/main.c
@@ -2,5 +2,14 @@
 
 int main(){
     InitWindow(800, 600, "Wordle Clone");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Testing Raylib.", 20, 20, 20, DARKBLUE);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
