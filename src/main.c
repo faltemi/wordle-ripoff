cda28165798888ab7522a90d4ec37c24bf1de7ca
faltemi <michael.falter5@gmail.com>
2025-08-17 19:43:25 -0400

    blinking title screen and array of letter cells

--- src/main.c
+++ src/main.c
@@ -1,14 +1,30 @@
 #include "raylib.h"
+#include <stddef.h>
+
+#define PLAYER_GUESSES  6
+#define NUM_LETTERS     5
 
 typedef enum GameScreen { LOGO, TITLE, GAMEPLAY, ENDING } GameScreen;
 
+// LetterCell Structure
+typedef struct LetterCell {
+    Vector2 position;
+    Vector2 size;
+    Rectangle bounds;
+    bool active;
+    char letter;
+} LetterCell;
+
 int main(){
+    // Initialization
+    // ----------------------------------------------------------------
     // Window setup parameters
     const int screenWidth = 800;
     const int screenHeight = 600;
     const char *windowTitle = "Wordle Clone";
 
     InitWindow(screenWidth, screenHeight, windowTitle);
+    // NOTE: Load resources (textures, fonts, audio) after Window initialization
     
     // Setup initial game state
     GameScreen screen = LOGO;
@@ -16,8 +32,21 @@ int main(){
     int framesCounter = 0;
     int gameResult = -1;
 
+    LetterCell letterCells[PLAYER_GUESSES][NUM_LETTERS] = { 0 };
+    // Initialize letter cells
+    for (int r = 0; r < PLAYER_GUESSES; r++){
+        for (int c = 0; c < NUM_LETTERS; c++){
+            letterCells[r][c].active = false;
+            letterCells[r][c].letter = '\0';
+            letterCells[r][c].size = (Vector2){ screenWidth/NUM_LETTERS, 20 };
+            letterCells[r][c].position = (Vector2){ c*letterCells[r][c].size.x, r*letterCells[r][c].size.y + 100 }; // Add y offset
+            letterCells[r][c].bounds = (Rectangle){ letterCells[r][c].position.x, letterCells[r][c].position.y, letterCells[r][c].size.x, letterCells[r][c].size.y };
+        }
+    }
+
     // Desired framerate
     SetTargetFPS(60);
+    // ----------------------------------------------------------------
 
     // Main loop
     while(!WindowShouldClose()){
@@ -69,7 +98,9 @@ int main(){
                 {
                     DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
                     DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
-                    DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+                    // Every half second toggle text (60 fps)
+                    if ((framesCounter/30)%2 == 0)
+                        DrawText("PRESS [ENTER] to START", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] to START", 20)/2, GetScreenHeight()/2 + 60, 20, DARKGRAY);
                 } break;
                 case GAMEPLAY:
                 {
@@ -90,9 +121,9 @@ int main(){
     }
 
     // De-Initialize
-    // ------------------------------------------------------------
+    // ----------------------------------------------------------------
 
     CloseWindow();
-    // ------------------------------------------------------------
+    // ----------------------------------------------------------------
     return 0;
 }
